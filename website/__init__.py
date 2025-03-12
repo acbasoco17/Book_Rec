2415cb385813546db4c123206f374144d8c5ad21
acbasoco17 <anacatherinebasoco@Anas-MacBook-Pro-2.local>
2025-03-11 22:34:10 -0400

    Creating Home Page

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,9 @@ from . import private
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = private.SECRET_KEY
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix = "/")
+
     return app
