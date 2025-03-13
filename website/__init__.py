4eaf9ed3a0a6b9b2e3b94a8577c8f71b5474b636
acbasoco17 <anacatherinebasoco@Anas-MacBook-Pro-2.local>
2025-03-13 18:34:51 -0400

    Added sign_in page template

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,9 @@ def create_app():
     app.config['SECRET_KEY'] = private.SECRET_KEY
 
     from .views import views
+    from.auth import auth
 
     app.register_blueprint(views, url_prefix = "/")
+    app.register_blueprint(auth, url_prefix = "/")
 
     return app
