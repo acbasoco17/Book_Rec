999e98e9f36a5d28948398d0d7f0ff513217f2fa
acbasoco17 <anacatherinebasoco@Anas-MacBook-Pro-2.local>
2025-03-13 18:42:56 -0400

    Added register template

--- website/auth.py
+++ website/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/sign_in')
 def sign_in():
     return render_template("sign_in.html")
+
+@auth.route('/register')
+def register():
+    return render_template("register.html")
